36fe46ddf3de465667e6aee383f05da703945df9
Zack <zacharymedeiros_cs@outlook.com>
2024-09-27 13:12:48 -0700

    Finish toy app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,9 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    #@user = User.first.microposts.first
+    #@user = User.find(params[:id])
+    #@micropost = @user.microposts.first
   end
 
   # GET /users/new
